8d05ef789e707a8e88f87b39b96be6ec74e52fba
erlendromo <erlendromo@hotmail.com>
2026-08-21 14:11:53 +0200

    feat: Added accent colors for hover mechanisms to theme

--- src/utils/theme/theme.go
+++ src/utils/theme/theme.go
@@ -3,15 +3,21 @@ package theme
 import "context"
 
 type Mode struct {
-	Name                string `json:"name"`
-	PrimaryBackground   string `json:"primary_background"`
-	SecondaryBackground string `json:"secondary_background"`
-	PrimaryText         string `json:"primary_text"`
-	SecondaryText       string `json:"secondary_text"`
-	PrimaryBorder       string `json:"primary_border"`
-	SecondaryBorder     string `json:"secondary_border"`
-	Accent              string `json:"accent"`
-	IconFilter          string `json:"icon_filter"`
+	Name                 string `json:"name"`
+	PrimaryBackground    string `json:"primary_background"`
+	SecondaryBackground  string `json:"secondary_background"`
+	PrimaryText          string `json:"primary_text"`
+	SecondaryText        string `json:"secondary_text"`
+	PrimaryBorder        string `json:"primary_border"`
+	SecondaryBorder      string `json:"secondary_border"`
+	AccentText           string `json:"accent_text"`
+	AccentHoverText      string `json:"accent_hover_text"`
+	AccentGroupHoverText string `json:"accent_group_hover_text"`
+	AccentFocusRing      string `json:"accent_focus_ring"`
+	AccentGradient       string `json:"accent_gradient"`
+	HoverBackground      string `json:"hover_background"`
+	HoverBorder          string `json:"hover_border"`
+	IconFilter           string `json:"icon_filter"`
 }
 
 type Theme interface {
